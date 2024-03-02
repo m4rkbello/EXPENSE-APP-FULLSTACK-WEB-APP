ae6732d84f447f831b248963f0386c8c704c54a6
m4rkbello <markbello10041999@gmail.com>
2024-03-03 01:26:30 +0800

    AUTHENTICATION WITH SANCTUM: REGISTER, LOGIN AND LOGOUT - FIXED!

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,80 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('m4rkbellofullstack')->plainTextToken;
+
+        $response = [
+            'success' => true,
+            'message' => 'User has new token!',
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password))
+        {
+            return response([
+                'message' => 'email or password is incorrect!'
+            ], 401);
+        };
+
+        $token = $user->createToken('m4rkbellofullstack')->plainTextToken;
+
+        $response = [
+            'success' => true,
+            'message' => 'Email and Password is correct!',
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+    }
+
+    // public function logout(Request $request)
+    // {
+    //     auth()->user()->token()->delete();
+    //     return [
+    //         "message" => 'Logout successfully!'
+    //     ];
+    // }
+
+    public function logout(Request $request)
+{
+    Auth::user()->currentAccessToken()->delete();
+    return [
+        "message" => 'Logout successfully!'
+    ];
+}
+   
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +18,20 @@ use App\Http\Controllers\StudentController;
 */
 
 Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
 Route::get('/students/{id}', [StudentController::class, 'show']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 Route::get('/students/search/{name}', [StudentController::class, 'search']);
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+});
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
