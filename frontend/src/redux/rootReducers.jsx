709e816d0a4ec56dff032849b4c9e861dc438fa6
m4rkbello <markbello10041999@gmail.com>
2024-03-04 21:31:53 +0800

    FRONTEND REDUX: ADD ROOT REDUCER

--- frontend/src/redux/rootReducers.jsx
+++ frontend/src/redux/rootReducers.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import studentReducer from '../redux/reducer';
+import loginReducer from '../redux/reducers/loginReducer'
 
 const rootReducer = combineReducers({
     studentReducer: studentReducer,
+    loginReducer: loginReducer,
     // Add other reducers here if needed
 });
 
