a994223fbb372031065fff36d9ab4a607d199ca7
m4rkbello <markbello10041999@gmail.com>
2024-03-05 00:55:59 +0800

    REDUX CONFIGURE FOR LOGIN - FIXED!

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,48 +1,30 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from '../Services/Api';
+import { connect } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { createUserPost } from '../redux/actions/loginActions';
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = ({ createUserPost }) => {
+  const [localEmail, setLocalEmail] = useState("");
+  const [localPassword, setLocalPassword] = useState("");
+
   const navigate = useNavigate();
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
+  const handleCreateUser = async (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/login', { email, password });
-
-      console.log("LOGIN SUCCESS!:", response.data);
+      await createUserPost({ email: localEmail, password: localPassword });
+      toast.success("User login successfully!");
       window.location.reload();
       navigate("/home");
-      // Check if login was successful
-      if (response.data && response.data.success) {
-        const { token } = response.data;
-
-        // Save token to localStorage
-        localStorage.setItem('M4rkbelloFullstackPersonalAccessToken', token);
-
-        // Save token to cookie
-        document.cookie = `M4rkbelloFullstackPersonalAccessToken=${token}; expires=${new Date(Date.now() + 86400 * 1000).toUTCString()}; path=/`;
-
-        // Clear email and password fields
-        setEmail("");
-        setPassword("");
-
-        // Navigate to the desired route
-
-      }
-      toast.success("Login successful!");
-
 
     } catch (error) {
-      console.log("ERROR SA PAG LOGIN", error);
-      // Handle network or other errors
-      toast.error("ERROR KOL!!");
+      toast.error("Email or Password is incorrect!");
+   
     }
   };
+  
 
   return (
     <div>
@@ -51,26 +33,25 @@ const Login = () => {
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">login</div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 md:flex">
-            <form onSubmit={handleLogin} className="card-body">
+            <form className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" className="input input-bordered" required />
-
+                <input type="email" value={localEmail} onChange={(e) => setLocalEmail(e.target.value)} placeholder="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" className="input input-bordered" required />
-
+                <input type="password" value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} placeholder="password" className="input input-bordered" required />
                 <label className="label">
                   <Link to="/home" className="label-text-alt link link-hover">Forgot password?</Link>
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary" onClick={handleCreateUser}>Login</button>
+                
               </div>
             </form>
           </div>
@@ -80,4 +61,13 @@ const Login = () => {
   )
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  // email: state.login.email, // Assuming you have a reducer called 'login' that holds email and password
+  // password: state.login.password,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  createUserPost: (userData) => dispatch(createUserPost(userData))
+});
+
+export default connect(null, mapDispatchToProps)(Login);
