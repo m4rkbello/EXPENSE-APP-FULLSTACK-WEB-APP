4583442af6f4fd2f65b84f2b75754e6a205abb57
m4rkbello <markbello10041999@gmail.com>
2024-03-21 20:45:18 +0800

    FRONTEND RESPONSIVE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,71 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes, Link, Navigate } from 'react-router-dom';
-import { connect, useSelector, useDispatch } from 'react-redux';
+import { Route, Routes, Link } from 'react-router-dom';
+import { connect, useSelector } from 'react-redux';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Wallet from './Components/Student';
 import Sidebar from './Components/SideBar';
-
 import ResetPassword from './Components/User/ResetUser/FindUserEmail';
 import NotFound from './Components/Pages/404';
 import { AiOutlineMenu, AiFillHome } from "react-icons/ai";
-import { fetchUserRequest, loginUserPost } from './redux/actions/userActions'
+import { fetchUserRequest } from './redux/actions/userActions';
 import UserProfile from './Components/User/Profile/UserProfile';
-
 import GetLocLongLat from './Services/GetLocLongLat'; './Services/GetLocation';
 
 function App(props) {
-  console.log("HOY GANA!", props&&props);
-
- 
   const [hasToken, setHasToken] = useState('');
   const [isAuthLocalStorage, setIsAuthLocalStorage] = useState(null);
 
   const isAuthenticatedUser = useSelector(state => state.userReducer.loginResponse.user);
 
-  console.log("USESELECTOR REDUX", isAuthenticatedUser);
-
   useEffect(() => {
     // Check if token exists in localStorage or cookies
     const token = localStorage.getItem('M4rkbelloFullstackPersonalAccessToken') && getCookie('M4rkbelloFullstackPersonalAccessToken');
-  
-    console.log("Token value:", token);
-  
+
     if (token) {
       setHasToken(true);
-      console.log("hasToken value after setting:", hasToken);
     }
-  
+
     props.fetchUserRequest();
   }, []);
-  
+
   function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
   }
-  
+
   const userCollection = props?.userData?.loginResponse?.users;
-  console.log("USER NA !", userCollection);
-  
+
   useEffect(() => {
     if (isAuthenticatedUser !== undefined && isAuthenticatedUser !== null) {
       const authUserToLocalStorage = JSON.stringify(isAuthenticatedUser);
       localStorage.setItem('userAuthenticatedByLocalStorage', authUserToLocalStorage);
       sessionStorage.setItem('userAuthenticatedBySessionStorage', authUserToLocalStorage);
     }
-  
+
     const storedFromLocalStorageData = localStorage.getItem('userAuthenticatedByLocalStorage');
     let parsedUserData = null;
-  
+
     if (storedFromLocalStorageData) {
       parsedUserData = JSON.parse(storedFromLocalStorageData);
       const extractedProperties = getUserProperties(parsedUserData);
       setIsAuthLocalStorage(extractedProperties);
     }
   }, [isAuthenticatedUser]);
-  
+
   const getUserProperties = (userData) => {
     if (userData) {
       const { id, name, email, password } = userData;
@@ -85,19 +74,19 @@ function App(props) {
     }
     return filteredUsers;
   }
-  
+
   const userAuthenticated = getUserAuthenticated(userCollection);
 
   return (
     <div className='shadow-2xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% min-h-screen'>
-    <div className="navbar shadow-2xl ..  bg-black">
-        {hasToken.length !== 0 && hasToken ? (
-        
-          
+      <div className="navbar shadow-2xl bg-black flex justify-between items-center px-4 py-2">
+        {hasToken && hasToken.length !== 0 ? (
           <div className="drawer">
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-              <label htmlFor="my-drawer" className="btn btn-primary drawer-button">   <AiOutlineMenu /> Menu</label>
+              <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+                <AiOutlineMenu /> Menu
+              </label>
             </div>
             <div className="shadow-2xl drawer-side">
               <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
@@ -119,27 +108,25 @@ function App(props) {
           <div className="flex-1">
             <Link to="/" className="btn btn-ghost text-xl">pullsnack</Link>
           </div>
-        )
-        }
+        )}
         <div className="flex-none" style={{ display: 'flex' }}>
           <ul className="menu menu-horizontal px-1">
-          {hasToken && userAuthenticated && userAuthenticated.length > 0 ? (
+            {hasToken && userAuthenticated && userAuthenticated.length > 0 ? (
               <>
-              {userAuthenticated.map((user, index) => (
-                <span className='text-3xl pt-3 pe-3' key={index}>
-                {user.name}
-                </span>
-              ))}
-              <div>
-              <Link to="/profile">
-              <div className="avatar online">
-              <div className="w-14 rounded-full">
-       
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-              </div>
-              </Link>
-              </div>
+                {userAuthenticated.map((user, index) => (
+                  <span className='text-3xl pt-3 pe-3' key={index}>
+                    {user.name}
+                  </span>
+                ))}
+                <div>
+                  <Link to="/profile">
+                    <div className="avatar online">
+                      <div className="w-14 rounded-full">
+                        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                      </div>
+                    </div>
+                  </Link>
+                </div>
               </>
             ) : (
               <div>
@@ -147,11 +134,11 @@ function App(props) {
                   <li className='shadow-2xl'>
                     <Link to="/login">Login</Link>
                   </li>
-                  <li className='shadow-2xl '>
+                  <li className='shadow-2xl'>
                     <Link to="/register">Register</Link>
                   </li>
-                  <li className='shadow-2xl '>
-                </li>
+                  <li className='shadow-2xl'>
+                  </li>
                 </ul>
               </div>
             )}
@@ -165,7 +152,7 @@ function App(props) {
             <>
               <Route path="/home" element={<Home />} />
               <Route path="/wallet" element={<Wallet />} />
-              <Route path="profile" element={<UserProfile />} />
+              <Route path="/profile" element={<UserProfile />} />
               <Route path="/getlocation" element={<GetLocLongLat />} />
               <Route path="/sidebar" element={<Sidebar />} />
             </>
@@ -174,7 +161,6 @@ function App(props) {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/resetpassword" element={<ResetPassword />} />
-
             </>
           )}
         </Routes>
@@ -183,23 +169,17 @@ function App(props) {
   );
 }
 
-
 function mapStateToProps(state) {
   console.log("DATA SA MAPTOSTATETOPROPS", state.userReducer);
   return {
-      userData: state.userReducer
+    userData: state.userReducer
   };
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchUserRequest: () => dispatch(fetchUserRequest()),
-    // loginResponse: () => dispatch(loginResponse()),
-    // loginUserPost: (userData) => dispatch(loginUserPost(userData)),
   };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-
