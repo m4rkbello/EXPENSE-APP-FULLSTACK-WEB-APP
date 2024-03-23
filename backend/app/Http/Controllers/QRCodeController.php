96333530474477972ec853345188326f9caad8a6
m4rkbello <markbello10041999@gmail.com>
2024-03-23 18:59:04 +0800

    ADD SIMPLE QRCODE FOR REGISTER AND LOGIN - FIXED

--- backend/app/Http/Controllers/QRCodeController.php
+++ backend/app/Http/Controllers/QRCodeController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\qrcode;
 
 use Illuminate\Http\Request;
 
 class QRCodeController extends Controller
 {
-    //
+    public function index()
+    {
+        return qrcode::all();
+    }
 }
