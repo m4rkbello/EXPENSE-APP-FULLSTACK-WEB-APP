3d3d131fbae03ef95ce04944c91849b6d233fc54
m4rkbello <markbello10041999@gmail.com>
2024-03-23 16:01:46 +0800

    ADD QRCODES MODEL/MIGRATIONS ANG FOREIGN KEYS FOR USERS AND STUDENTS

---
+++ backend/database/migrations/2024_03_23_065619_create_qrcodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('qrcodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('qrc_uuid');
+            $table->unsignedBigInteger('qrc_user_id');
+            $table->foreign('qrc_user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('qrc_student_id');
+            $table->foreign('qrc_student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('qrcodes');
+    }
+};
