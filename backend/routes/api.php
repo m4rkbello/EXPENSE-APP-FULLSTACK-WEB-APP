84520b54be779dc7538006707ccdd4d23a0615ef
m4rkbello <markbello10041999@gmail.com>
2024-03-24 14:53:00 +0800

    BACKEND: add a route using any method request

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,7 +24,11 @@ Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::get('/students/search/{name}', [StudentController::class, 'search']);
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/scan-qrcode', [AuthController::class, 'verify_qrcode']);
+// Route::post('/scan-qrcode', [AuthController::class, 'verify_qrcode']);
+Route::any('/scan-qrcode', function () {
+    return response()->json(['message' => 'Scan QR Code route hit']);
+});
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/find-user', [AuthController::class, 'findUserEmail']);
 Route::put('/resetpassword/{email}', [AuthController::class, 'newUserPassword']);
